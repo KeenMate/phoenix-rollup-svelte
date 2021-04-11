1248dd7863622d40b113d17c3b06f2c4e2de084d
Albert Moravec <albert.moravec@gmail.com>
2021-04-11 21:12:10 +0200

    Fix purge

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -3,7 +3,10 @@ const production = process.env.MIX_ENV == "prod";
 module.exports = {
   purge: {
     enabled: production,
-    content: ["./apps/**/*.svelte"]
+    content: [
+      "./apps/**/*.svelte",
+      "../lib/rollup_test_web/templates/**/*.html.eex"
+    ]
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
