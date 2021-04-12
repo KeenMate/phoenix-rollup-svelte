1e1d689b1a8eb474bd87c12557173d47cb746e02
Ondrej Valenta <ondrej.valenta@keenmate.com>
2021-04-12 18:13:40 +0200

    New way for css bindings to survive tailwind purge

--- assets/apps/connect/js/App.svelte
+++ assets/apps/connect/js/App.svelte
@@ -9,9 +9,7 @@
 </script>
 
 <button
-  class:bg-indigo-500="{connected}"
-  class:bg-pink-500="{!connected}"
-  class="active:bg-pink-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1"
+  class="{connected ? 'bg-indigo-500': 'bg-pink-500'} active:bg-pink-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1"
   type="button"
   style="transition: all 0.15s ease 0s;"
   on:click="{connect}"
