09d1ccb79f2e6d6147c66dd031cbf936b653dfbb
Albert Moravec <albert.moravec@gmail.com>
2021-04-22 23:40:56 +0200

    Rename partials and fix name

--- lib/rollup_test_web/views/page_view.ex
+++ lib/rollup_test_web/views/page_view.ex
@@ -1,7 +1,7 @@
 defmodule RollupTestWeb.PageView do
   use RollupTestWeb, :view
 
-  def page_title("index.html", _), do: "Ondra's Boobies"
+  def page_title("index.html", _), do: "Demo page"
 
   def page_title("another_page.html", %{number: num}), do: "Order: #{num}"
 end
